408edea4c7fe88dca56010d4900bb1103bfdef34
aswati95 <146626345+aswati95@users.noreply.github.com>
2023-10-06 20:31:53 +0530

    Update 04_dataTypeSummary.js

--- 01_basics.js/04_dataTypeSummary.js
+++ 01_basics.js/04_dataTypeSummary.js
@@ -2,36 +2,36 @@
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+const score = 100        //number
+const scoreValue = 100.3   //number
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+const isLoggedIn = false     /boolean
+const outsideTemp = null      //object
+let userEmail;                //undefined
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+const id = Symbol('123')               //symbol
+const anotherId = Symbol('123')       //symbol
 
 console.log(id === anotherId);
 
-// const bigNumber = 3456543576654356754n
+// const bigNumber = 3456543576654356754n  (datatype = undefined)
 
 
 
 // Call By Reference (Non primitive)
 
-// Array, Objects, Functions
+// Array, Objects, Functions (datatype = object , for function = function object)
 
 const heros = ["shaktiman", "naagraj", "doga"];
 let myObj = {
     name: "hitesh",
     age: 22,
-}
+}                                                     //object
 
 const myFunction = function(){
     console.log("Hello world");
-}
+}                                                     //function object
 
-console.log(typeof anotherId);
+console.log(typeof anotherId);                       
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
